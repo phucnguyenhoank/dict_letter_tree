8f0d89f1fd3f3f7430d7f8aa6e0089916ccc8193
HoangPhuc <phucnguyeho@gmail.com>
2023-10-21 22:53:46 +0700

    improve user input when suggest words

--- Main.cpp
+++ Main.cpp
@@ -1,5 +1,6 @@
 ﻿#include <fstream>
 #include "LettersTree.h"
+#include <conio.h>
 
 
 
@@ -9,18 +10,65 @@ int main() {
 
     std::ifstream fin("words.txt");
     if (fin.fail()) return 1;
-    
     std::string word;
-    while (std::getline(fin, word)) {
-        lt->addLetter(word);
-    }
-
+    while (std::getline(fin, word)) lt->addLetter(word);
     fin.close();
 
-    // lt->show();
-    lt->crushWords();
-    std::cout << "-----------------------------\n";
-    lt->crushWords("asb");
+    // PHIÊN BẢN NGƯỜI DÙNG NHẬP Ở CUỐI DANH SÁCH GỢI Ý
+    // TRONG TRƯỜNG HỢP LIST TỪ DÀI
+    //
+    //std::string userWord;
+
+    ///*
+    //ENTER: 13
+    //BACKSPACE: 8
+    //SPACE: 32
+    //'\n': 10
+    //*/
+    //char userChar = 14;
+
+    //do {
+    //    userChar = _getch();
+    //    if (userChar == 8) {
+    //        if (userWord.size() == 0) continue;
+    //        userWord.pop_back();
+    //    }
+    //    else {
+    //        userWord += userChar;
+    //    }
+    //    
+    //    system("cls");
+    //    if (userWord.size()) lt->crushWords(userWord);
+    //    std::cout << "enter a word: " << userWord;
+    //} while (userChar != 13);
+
+
+    /*
+    ENTER: 13
+    BACKSPACE: 8
+    SPACE: 32
+    '\n': 10
+    */
+    std::string userWord;
+
+    char userChar = 14;
+
+    do {
+        system("cls");
+        std::cout << "enter a word: " << userWord << "_\n";
+        
+
+        if (userWord.size()) lt->crushWords(userWord);
+        userChar = _getch();
+        if (userChar == 8) {
+            if (userWord.size() == 0) continue;
+            userWord.pop_back();
+        }
+        else {
+            userWord += userChar;
+        }
+
+    } while (userChar != 13);
 
     
 
